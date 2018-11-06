f255971ca003df235306d0b65d8ce1f39fb65027
Arnav <aggarnav@gmail.com>
2018-11-06 22:50:06 +0530

    first commit

---
+++ train.py
@@ -0,0 +1,116 @@
+import dataset
+import tensorflow as tf
+import time
+from datetime import timedelta
+import math
+import random
+import numpy as np
+import os
+import NetworkBuilder
+
+#Adding Seed so that random initialization is consistent
+from numpy.random import seed
+seed(1)
+from tensorflow import set_random_seed
+set_random_seed(2)
+
+
+batch_size = 32
+
+#Prepare input data
+classes = os.listdir('training_data/')
+print(classes)
+num_classes = len(classes)
+
+# 10% of the data will automatically be used for validation
+validation_size = 0.1
+img_size = 128
+num_channels = 3
+train_path='training_data/'
+
+# We shall load all the training and validation images and labels into memory using openCV and use that during training
+data = dataset.read_train_sets(train_path, img_size, classes, validation_size=validation_size)
+
+
+print("Complete reading input data. Will Now print a snippet of it")
+print("Number of files in Training-set:\t\t{}".format(len(data.train.labels)))
+print("Number of files in Validation-set:\t{}".format(len(data.valid.labels)))
+
+
+
+session = tf.Session()
+x = tf.placeholder(tf.float32, shape=[None, img_size,img_size,num_channels], name='x')
+
+## labels
+y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
+y_true_cls = tf.argmax(y_true, dimension=1)
+
+
+
+def create_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+def create_biases(size):
+    return tf.Variable(tf.constant(0.05, shape=[size]))
+nb = NetworkBuilder.NetworkBuilder()
+
+with tf.name_scope("ModelV2") as scope:
+    model = x
+    model = nb.flatten(model)
+    model = nb.attach_relu_layer(model)
+    model = nb.attach_sigmoid_layer(model)
+    model = nb.attach_dense_layer(model, 3)
+
+
+y_pred = tf.nn.softmax(model,name='y_pred')
+
+y_pred_cls = tf.argmax(y_pred, dimension=1)
+session.run(tf.global_variables_initializer())
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=model,
+                                                    labels=y_true)
+cost = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+correct_prediction = tf.equal(y_pred_cls, y_true_cls)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+session.run(tf.global_variables_initializer()) 
+
+
+def show_progress(epoch, feed_dict_train, feed_dict_validate, val_loss):
+    acc = session.run(accuracy, feed_dict=feed_dict_train)
+    val_acc = session.run(accuracy, feed_dict=feed_dict_validate)
+    msg = "Training Epoch {0} --- Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%},  Validation Loss: {3:.3f}"
+    print(msg.format(epoch + 1, acc, val_acc, val_loss))
+
+total_iterations = 0
+
+all_saver = tf.train.Saver(save_relative_paths=True)
+def train(num_iteration):
+    global total_iterations
+    
+    for i in range(total_iterations,
+                   total_iterations + num_iteration):
+
+        x_batch, y_true_batch, _, cls_batch = data.train.next_batch(batch_size)
+        x_valid_batch, y_valid_batch, _, valid_cls_batch = data.valid.next_batch(batch_size)
+
+        
+        feed_dict_tr = {x: x_batch,
+                           y_true: y_true_batch}
+        feed_dict_val = {x: x_valid_batch,
+                              y_true: y_valid_batch}
+
+        session.run(optimizer, feed_dict=feed_dict_tr)
+
+        if i % int(data.train.num_examples/batch_size) == 0: 
+            val_loss = session.run(cost, feed_dict=feed_dict_val)
+            epoch = int(i / int(data.train.num_examples/batch_size))    
+            
+            show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
+            all_saver.save(session, './Forest-model')
+
+
+    total_iterations += num_iteration
+
+train(num_iteration=6000)
